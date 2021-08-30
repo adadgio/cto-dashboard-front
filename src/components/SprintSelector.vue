52d8ebf9754fc85f36b764066a01ad38546a3ee2
S-Lauren <sarah.ghanem.simplon@gmail.com>
2021-08-30 16:40:36 +0200

    try to type selector

--- src/components/SprintSelector.vue
+++ src/components/SprintSelector.vue
@@ -1,45 +1,132 @@
+
+
 <template>
-  <div class="selector">Selector
+  <div class="custom-select" :tabindex="tabindex" @blur="open = false">
+    <div class="selected" :class="{ open: open }" @click="open = !open">
+      {{ selected }}
+    </div>
+    <div class="items" :class="{ selectHide: !open }">
+      <div
+        v-for="(option, i) of options"
+        :key="i"
+        @click="
+          selected = option;
+          open = false;
+          $emit('input', option);
+        "
+      >
+        {{ option }}
+      </div>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
-import { Options, Vue } from 'vue-class-component';
+declare interface SprintSelectorTypes {
+  selected?: string;
+  open?: boolean;
+  default?: string;
+  options?: string;
+}
 
-@Options({
+export default {
   props: {
-    msg: String
+    options: {
+      type: Array,
+      required: true,
+    },
+    default: {
+      type: String,
+      required: false,
+      default: null,
+    },
+    tabindex: {
+      type: Number,
+      required: false,
+      default: 0,
+    },
   },
   data() {
-      return {
-      }
+    return {
+      selected: this.default
+        ? this.default
+        : this.options.length > 0
+        ? this.options[0]
+        : null,
+      open: false,
+    } as any 
+  }, 
+
+  mounted() {
+    this.$emit("input", this.selected);
   },
-  computed: {
-      
-  },
-  methods: {
-      
-  }
-})
-export default class HelloWorld extends Vue {
-  //msg!: string;
-}
+};
+
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-h3 {
-  margin: 40px 0 0;
+.custom-select {
+  position: relative;
+  width: 100%;
+  text-align: left;
+  outline: none;
+  height: 47px;
+  line-height: 47px;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+
+.custom-select .selected {
+  background-color: #0a0a0a;
+  border-radius: 6px;
+  border: 1px solid #666666;
+  color: #fff;
+  padding-left: 1em;
+  cursor: pointer;
+  user-select: none;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+
+.custom-select .selected.open {
+  border: 1px solid #ad8225;
+  border-radius: 6px 6px 0px 0px;
 }
-a {
-  color: #42b983;
+
+.custom-select .selected:after {
+  position: absolute;
+  content: "";
+  top: 22px;
+  right: 1em;
+  width: 0;
+  height: 0;
+  border: 5px solid transparent;
+  border-color: #fff transparent transparent transparent;
+}
+
+.custom-select .items {
+  color: #fff;
+  border-radius: 0px 0px 6px 6px;
+  overflow: hidden;
+  border-right: 1px solid #ad8225;
+  border-left: 1px solid #ad8225;
+  border-bottom: 1px solid #ad8225;
+  position: absolute;
+  background-color: #0a0a0a;
+  left: 0;
+  right: 0;
+  z-index: 1;
+}
+
+.custom-select .items div {
+  color: #fff;
+  padding-left: 1em;
+  cursor: pointer;
+  user-select: none;
+}
+
+.custom-select .items div:hover {
+  background-color: #ad8225;
+}
+
+.selectHide {
+  display: none;
 }
 </style>
