109997cf8e1ee7016f9d455a32f981ce3ceda9c2
adadgio <romain.bruckert.esc@gmail.com>
2021-08-30 14:50:52 +0200

    First commit.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <HelloWorld :msg="'Hi'"/>
   </div>
 </template>
 
@@ -13,6 +12,16 @@ import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
   components: {
     HelloWorld,
   },
+  data() {
+      return {
+        
+      }
+  },
+  methods: {
+      stuffHasBeenChange() {
+        
+      }
+  }
 })
 export default class Home extends Vue {}
 </script>
