7de0f8cb21d424509696eee3cac24f7ce31ff325
S-Lauren <sarah.ghanem.simplon@gmail.com>
2021-08-31 17:33:24 +0200

    come back please

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -10,17 +10,17 @@
         <ProjectList :projects="[
             {
                 'name': 'Projet de test 1',
-                'nbBugsTodo': '2',
-                'nbBugsDone': '3',
-                'nbFeatureTodo': '1',
-                'nbFeatureDone': '4',
+                'nbBugsTodo': 2,
+                'nbBugsDone': 3,
+                'nbFeatureTodo': 1,
+                'nbFeatureDone': 4,
             },
             {
                 'name': 'Projet de test 2',
-                'nbBugsTodo': '4',
-                'nbBugsDone': '6',
-                'nbFeatureTodo': '0',
-                'nbFeatureDone': '7',
+                'nbBugsTodo': 4,
+                'nbBugsDone': 6,
+                'nbFeatureTodo': 0,
+                'nbFeatureDone': 7,
             }
         ]" />
     </div>
