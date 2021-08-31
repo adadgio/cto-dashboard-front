a77927e858b3ebeed4076725f1b6bb33952290b4
Dotpunkto <Dotpunkt0@vivaldi.net>
2021-08-31 17:15:20 +0200

    rebase develop into ProjectDetails

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,18 +7,45 @@
         <ProjectList :projects="[
             {
                 'name': 'Projet de test 1',
-                'nbBugsTodo': '2',
-                'nbBugsDone': '3',
-                'nbFeatureTodo': '1',
-                'nbFeatureDone': '4',
+                'nbBugsTodo': 2,
+                'nbBugsDone': 3,
+                'nbFeatureTodo': 1,
+                'nbFeatureDone': 4,
             },
             {
                 'name': 'Projet de test 2',
-                'nbBugsTodo': '4',
-                'nbBugsDone': '6',
-                'nbFeatureTodo': '0',
-                'nbFeatureDone': '7',
+                'nbBugsTodo': 4,
+                'nbBugsDone': 6,
+                'nbFeatureTodo': 0,
+                'nbFeatureDone': 7,
             }
+        ]" />
+        <ProjectDetails :projectName="'test'" :issues="[
+            {
+                'id': 1,
+                'name': 'azert',
+                'type': 'Bug',
+                'status': 'Done'
+            },
+            {
+                'id': 2,
+                'name': 'azert2',
+                'type': 'Bug',
+                'status': 'Todo'
+            },
+            {
+                'id': 3,
+                'name': 'azert3',
+                'type': 'Feature',
+                'status': 'Done'
+            },
+            {
+                'id': 4,
+                'name': 'azert4',
+                'type': 'Feature',
+                'status': 'Todo'
+            },
+            
         ]" />
     </div>
 
@@ -29,6 +56,7 @@ import { Options, Vue } from 'vue-class-component'
 import { Sprint } from '@cto-dashboard-model/cto-dashboard-model'
 import ProjectList from '@/components/ProjectList.vue'
 import SprintSelector from '@/components/SprintSelector.vue'
+import ProjectDetails from '@/components/ProjectDetails.vue'
 
 export interface SprintSelectorEvent {
     selection: Array<Sprint>
@@ -37,6 +65,7 @@ export interface SprintSelectorEvent {
     components: {
         ProjectList,
         SprintSelector,
+        ProjectDetails
     },
 
     data() {
@@ -59,3 +88,10 @@ export interface SprintSelectorEvent {
 })
 export default class Home extends Vue {}
 </script>
+
+<style scoped lang="scss">
+    .home {
+        display: flex;
+        flex-direction: row;
+    }
+</style>
