bbc6518ed36789ba848fac2d9716e4637757daeb
Theo <t.degraeuwe@leviatan.fr>
2021-08-31 13:55:18 +0200

    fixes for eslint

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,82 +1,36 @@
 <template>
-<<<<<<< HEAD
-  <div class="home">
-    <ProjectList :projects="[{
-                              'name':	'Projet de test 1',
-                              'nbBugsTodo': '2',
-                              'nbBugsDone': '3',
-                              'nbFeatureTodo': '1',
-                              'nbFeatureDone': '4',
-                              },
-                              {
-                              'name':	'Projet de test 2',
-                              'nbBugsTodo': '4',
-                              'nbBugsDone': '6',
-                              'nbFeatureTodo': '0',
-                              'nbFeatureDone': '7',
-                              }
-                            ]" />
-  </div>
-</template>
-
-<script lang="ts">
-import { Options, Vue } from 'vue-class-component';
-import ProjectList from '@/components/ProjectList.vue';
-
-@Options({
-  components: {
-    ProjectList
-  },
-  data() {
-      return {
-        
-      }
-  },
-  methods: {
-      stuffHasBeenChange() {
-        
-      }
-  }
-=======
     <div class="home">
-        <HelloWorld :msg="'Hi'"/>
-        <SprintSelector
-            :options="sprints"
-            @onChange="onSelectChange"
-        />
+        <ProjectList :projects="[
+            {
+                'name': 'Projet de test 1',
+                'nbBugsTodo': '2',
+                'nbBugsDone': '3',
+                'nbFeatureTodo': '1',
+                'nbFeatureDone': '4',
+            },
+            {
+                'name': 'Projet de test 2',
+                'nbBugsTodo': '4',
+                'nbBugsDone': '6',
+                'nbFeatureTodo': '0',
+                'nbFeatureDone': '7',
+            }
+        ]" />
     </div>
 </template>
 
 <script lang="ts">
 import { Options, Vue } from 'vue-class-component'
-import HelloWorld from '@/components/HelloWorld.vue'
-import SprintSelector from '@/components/SprintSelector.vue'
-import { SprintSelectorEvent } from '@/events/Events'
+import ProjectList from '@/components/ProjectList.vue'
 
 @Options({
     components: {
-        HelloWorld,
-        SprintSelector,
+        ProjectList,
     },
     data() {
-        return {
-            sprints: [
-                {
-                    id: 1, name: 'Sprint 1',
-                },
-                {
-                    id: 2, name: 'Sprint 2',
-                },
-            ],
-        }
+        return {}
     },
-    methods: {
-        onSelectChange(e: SprintSelectorEvent) {
-            // event received
-            console.log(e)
-        },
-    },
->>>>>>> develop
+    methods: {},
 })
 export default class Home extends Vue {}
 </script>
