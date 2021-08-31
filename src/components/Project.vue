0c697586d746df7b95686a55bd54709a23617321
Kevin Figueroa <kevin.figueroa@labo-ve.fr>
2021-08-31 14:03:07 +0200

    linted it

--- src/components/Project.vue
+++ src/components/Project.vue
@@ -2,33 +2,31 @@
     <div>
         <h2>{{ project.name }}</h2>
         <div>
-            <ProjectCard :onGoing="true" :project="project" />    
+            <ProjectCard :onGoing="true" :project="project" />
         </div>
         <div>
             <ProjectCard :onGoing="false" :project="project" />
         </div>
 
-        
     </div>
 </template>
 
-
 <script lang="ts">
-import { Options, Vue } from "vue-class-component";
-import ProjectCard from '@/components/SprintProjectCard.vue';
+import { Options, Vue } from 'vue-class-component'
+import ProjectCard from '@/components/SprintProjectCard.vue'
 
 @Options({
     components: {
-        ProjectCard
+        ProjectCard,
     },
     props: {
-        project :{
+        project: {
             type: Object,
-            default: function () {
-                return {    }
-            }
-        }
-      
+            default() {
+                return { }
+            },
+        },
+
     },
 })
 export default class Project extends Vue {
